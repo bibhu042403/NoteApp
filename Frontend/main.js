5cb76b779950002ba1a8c2b16f9e05e86672c0c2
bibhu042403 <bibhu.bhushan0403@gmail.com>
2022-02-13 17:47:26 +0530

    main.js is added

---
+++ Frontend/main.js
@@ -0,0 +1,16 @@
+const fetchNotes = () => {
+  const arr = [
+    {
+      notes: "take note",
+      author: "abc",
+    },
+    {
+      notes:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium vel unde natus nam expedita quis quibusdam dolor officiis inventore libero Repudiandae qui autem exercitationem voluptas ut optio tempore ratione esse.!",
+
+      author: "Bibhu",
+    },
+  ];
+
+  const noteListTag = document.getElementById("view-note-list");
+};
