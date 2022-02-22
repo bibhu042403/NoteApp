c99d751af0a3e76beac6bc93f47d5e0846843c65
bibhu042403 <bibhu.bhushan0403@gmail.com>
2022-02-22 14:01:08 +0530

    update

--- Backend/connectToDatabase.js
+++ Backend/connectToDatabase.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectToDatabase = () => {
   // Connect to data base
   mongoose.connect(
-    "mongodb+srv://bibhu0424:Mongo@042403@note-app.rpwzb.mongodb.net/Notes_data",
+    "mongodb+srv://bibhu:bibhu@netapp.plgz2.mongodb.net/Notes_data",
     () => {
       console.log("Connected to database");
     }
